f4f6557ddf2d5111e403b4ba459edf91797440f3
pablo mujica <pablomujica@pablos-MacBook-Pro-2.local>
2019-07-18 15:48:42 -0400

    Deleted duplicates "Show"

---
+++ spec/javascript/components/ShowLocationTile.js
@@ -0,0 +1,33 @@
+import ShowLocationTile from '../../../app/javascript/react/components/ShowLocationTile'
+import React from 'react';
+
+describe('ShowLocationTile', () => {
+  let name,
+      address,
+      city,
+      state,
+      zip,
+      wrapper
+
+      beforeEach(() => {
+        wrapper = mount (
+          <ShowLocationTile
+          name="Trillium"
+          address="That big street in Boston"
+          city="Boston"
+          state="MA"
+          zip="02420"
+          />
+        );
+    })
+
+  it("Should have an h1 tag", ()=>{
+    expect(wrapper.find('h1')).toBePresent();
+  });
+
+  it("Should have an p tag", ()=>{
+    expect(wrapper.find('p')).toBePresent();
+  });
+
+
+});
