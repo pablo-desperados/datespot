11f4f97e030acca3cf73c0768b4f0c9d2b61dc78
pablo mujica <pablomujica@pablos-MacBook-Pro-2.local>
2019-07-22 14:20:26 -0400

    Added Sign in and Sign up route logic

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,6 +5,14 @@ class ApplicationController < ActionController::Base
 
   before_action :configure_permitted_parameters, if: :devise_controller?
 
+  def after_sign_in_path_for(resource)
+    locations_path
+  end
+
+  def after_sign_up_path_for(resource)
+    locations_path
+  end
+
   protected
 
   def configure_permitted_parameters
