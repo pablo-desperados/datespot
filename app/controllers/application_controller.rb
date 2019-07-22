bda17c8488ccb8f73cbd6255cd2e802d1f6dbc6b
jodillion <dillionjoann@gmail.com>
2019-07-22 11:06:36 -0400

    added Carrier Wave and ability to add profile photo when signing up

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,7 +8,7 @@ class ApplicationController < ActionController::Base
   protected
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name, :profile_photo])
     devise_parameter_sanitizer.permit(:sign_in) do |user_params|
       user_params.permit(:email)
     end
