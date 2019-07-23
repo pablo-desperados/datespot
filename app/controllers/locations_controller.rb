75b6cace2705b6d9d89c669bef54d3477383edce
VictorVitkov <victor.vitkov@gmail.com>
2019-07-23 10:21:40 -0400

    wrote the edit for in rails

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -11,4 +11,24 @@ class LocationsController < ApplicationController
     @location = Location.find(params[:id])
   end
 
+  def edit
+    @location = Location.find(params[:id])
+  end
+
+  def update
+    @location = Location.find(params[:id])
+    if @location.update(location_params)
+      flash[:success] = 'Location was successfully updated.'
+      redirect_to @location
+    else
+      flash.now[:fail] = @location.errors.full_messages.join(", ")
+      render :edit
+    end
+  end
+
+  private
+
+  def location_params
+    params.require(:location).permit(:name, :address, :city, :state, :zip)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   root 'locations#index'
   devise_for :users
-  resources :locations, only: [:index, :new, :show]
+  resources :locations, only: [:index, :new, :show, :edit, :update]
 
   namespace :api do
     namespace :v1 do
