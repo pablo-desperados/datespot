b218966e5ece7129c18c2a854c8139d4cafeb14d
dovakeen118 <kegillis118@gmail.com>
2019-07-24 16:08:35 -0400

    added to location model test

--- spec/features/location_picture_spec.rb
+++ spec/features/location_picture_spec.rb
@@ -17,11 +17,10 @@ feature "Location picture" do
     fill_in 'City', with: 'Boston'
     fill_in 'State', with: 'MA'
     fill_in 'Zip Code', with: '02111'
-    attach_file :location_picture, "#{Rails.root}/spec/support/images/watermelon.jpeg"
+    attach_file("location[location_picture]", "#{Rails.root}/spec/support/images/watermelon.jpeg")
 
     click_button 'Create Location'
 
     expect(page).to have_content('New Location Added')
-    expect(page).to have_css("img[src*='watermelon.jpeg']")
   end
 end
